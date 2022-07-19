5a5268c69799851aa82964c0d342473c4d3eb9ca
Aishwarya Pakeyara <apakeyara@gmail.com>
2022-07-19 19:04:08 +0530

    App files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.all 
-    render json: {user: @users}
+    render json: {data: UserSerializer.new(@users, each_serializer: UserSerializer), 
+      message: "All Users fetched successfully", 
+      status: 200, 
+      type: 'Success'}
     end
 
   def new
@@ -12,7 +15,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save 
-      render json: {user: @user, message: "User created successfully"}
+      #render json: {user: @user, message: ""}
+      render json: {data: UserSerializer.new(@user, each_serializer: UserSerializer), 
+        message: "User created successfully", 
+        status: 200, 
+        type: 'Success'}
     else
       render json: {error: "Failure to create a User"}
     end
@@ -27,7 +34,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     
     if @user.update(user_params)
-      render json: {user: @user, message: "User updated successully"}
+      #render json: {user: @user, message: "User updated successully"}
+      render json: {data: UserSerializer.new(@user, each_serializer: UserSerializer), 
+        message: "User updated successully", 
+        status: 200, 
+        type: 'Success'}
     else
       render json: {error: "Failure to update the user details."}
     end   
@@ -37,7 +48,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.destroy
-      render json: {user: @user, message: "User successfully destroyed."}
+      # render json: {user: @user, message: "User successfully destroyed."}
+      render json: {data: UserSerializer.new(@user, each_serializer: UserSerializer), 
+        message: "User deleted successully", 
+        status: 200, 
+        type: 'Success'}
+
     else
       render json: {error: "Unable to delete user."}
     end
@@ -45,6 +61,6 @@ class UsersController < ApplicationController
 
   private 
   def user_params
-    params.require(:user).permit(:name, :age, :gender)
+    params.require(:user).permit(:name, :age, :gender, :email)
   end
 end
